5739104d2b624bd429458ce8fd5284d955e373f9
Tobias Koch <t.koch@tk-software.de>
2019-02-10 20:14:57 +0100

    Implementing Author class

--- src/Mjolnir/IO/Author.cs
+++ src/Mjolnir/IO/Author.cs
@@ -27,6 +27,10 @@
 
 #region Namespaces
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 #endregion
 
 namespace Mjolnir.IO
@@ -71,6 +75,76 @@ namespace Mjolnir.IO
         /// </summary>
         /// <value>The email address of the author.</value>
         public string EMailAddress { get; set; } = string.Empty;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Reads a list of authros from the given <paramref name="stream"/>.
+        /// </summary>
+        /// <param name="stream">The <see cref="Stream"/> containing the author list.</param>
+        /// <returns>An <see cref="IEnumerable{T}"/> containing the authors.</returns>
+        /// <exception cref="ArgumentNullException"><c>stream</c> is <c>null</c>.</exception>
+        /// <exception cref="IOException">Error while reading the stream.</exception>
+        public static IEnumerable<Author> From(Stream stream)
+        {
+            Task<IEnumerable<Author>> result = null;
+
+            try
+            {
+                result = FromAsync(stream);
+                Task.WaitAll(result);
+            }
+            catch (AggregateException aex)
+            {
+                aex.Handle((exception) =>
+                {
+                    if (exception is IOException)
+                    {
+                        throw exception as IOException;
+                    }
+
+                    return false;
+                });
+            }
+
+            return result?.Result;
+        }
+
+        /// <summary>
+        /// Reads a list of authros from the given <paramref name="stream"/>.
+        /// </summary>
+        /// <param name="stream">The <see cref="Stream"/> containing the author list.</param>
+        /// <returns>
+        /// A <see cref="Task{TResult}"/> that represents the asynchronous write operation.
+        /// The <c>TResult</c> parameter contains an <see cref="IEnumerable{T}"/>
+        /// containing the authors.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><c>stream</c> is <c>null</c>.</exception>
+        /// <exception cref="IOException">Error while reading the stream.</exception>
+        public static async Task<IEnumerable<Author>> FromAsync(Stream stream)
+        {
+            try
+            {
+                List<Author> authors = new List<Author>();
+                StreamReader reader = new StreamReader(stream);
+
+                IConfiguration configuration = new DefaultConfiguration();
+                string line;
+
+                while ((line = await reader.ReadLineAsync().ConfigureAwait(false)) != null)
+                {
+                    // TODO: Implement RegEx to read the authors
+                }
+
+                return authors;
+            }
+            catch (Exception ex)
+            {
+                throw new IOException("Error while reading the stream", ex);
+            }
+        }
 
         #endregion
     }
